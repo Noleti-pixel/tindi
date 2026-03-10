fd9595529645209c4e00a0fc690aead80fb64788
noletindi1 <noletindi1@gmail.com>
2026-03-10 18:58:15 +0300

    Save changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,57 +5,142 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
+          backgroundColor: Colors.blue,
           title: Text(
-            "Login Screen",
-            style: TextStyle(color: Colors.white, fontSize: 40),
+            "sign up",
+            style: TextStyle(fontSize: 30, color: Colors.white),
           ),
           centerTitle: true,
         ),
-        body: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset('assets/website.jpg', height: 200, width: 200),
-            Text(
-              "Jumia Marketplace",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 30,
-                fontWeight: FontWeight.w300,
-              ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/bird_2.jpg', width: 100, height: 100),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "username",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    hintText: "enter email",
+                    hintStyle: TextStyle(fontWeight: FontWeight.w100),
+                    prefixIcon: Icon(Icons.email, color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "password",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    hintText: "enter password",
+                    hintStyle: TextStyle(fontWeight: FontWeight.w100),
+                    prefixIcon: Icon(Icons.lock, color: Colors.black),
+                    suffixIcon: Icon(Icons.visibility_off, color: Colors.black),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "confirm password",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                    ),
+                    hintText: "confirm password",
+                    hintStyle: TextStyle(fontWeight: FontWeight.w100),
+                    prefixIcon: Icon(Icons.lock, color: Colors.black),
+                    suffixIcon: Icon(Icons.visibility_off, color: Colors.black),
+                  ),
+                ),
+                // MaterialButton(
+                //   onPressed: () {},
+                //   textColor: Colors.blueAccent,
+                //   child: Text(
+                //     "sign up",
+                //     style: TextStyle(fontSize: 20, color: Colors.black),
+                //   ),
+                // ),
+                Container(
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "sign up",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 0),
+                  child: Row(
+                    children: [
+                      Text("Already have an account?"),
+                      SizedBox(width: 5),
+                      Text("sign in", style: TextStyle(color: Colors.pink)),
+                      Spacer(),
+                      Text("Forgot password?"),
+                      SizedBox(width: 5),
+                      Text(
+                        "reset password",
+                        style: TextStyle(color: Colors.pink),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              "Login screen",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "Enter username",
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
-            ),
-            TextField(),
-            Text(
-              "Enter password",
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w200),
-            ),
-            /*Padding(
-              padding: const EdgeInserts,fromLTRB(20,0,20,0),
-              child: Container(
-                height: 50,
-                width: double,infinity,
-                alighnment.center,
-                decoration*/
-            TextField(),
-            SizedBox(height: 30),
-            MaterialButton(
-              onPressed: () {},
-              color: Colors.blue,
-              textColor: Colors.white,
-              child: Text("login"),
-            ),
-          ],
+          ),
         ),
       ),
     ),
